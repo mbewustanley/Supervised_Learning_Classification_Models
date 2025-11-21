c721a82a347af476d858145325ea49cbdbc28e1a
mbewustanley <mbewustanley@gmail.com>
2025-11-21 15:15:26 +0100

    adding knn model init, knn model in pkl and knn performance metrics

---
+++ src/Models/knn.py
@@ -0,0 +1,39 @@
+import json
+import numpy as np
+import pickle
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score
+
+#import data
+X_train = np.load('data/X_train.npy')
+y_train = np.load('data/y_train.npy')
+X_test = np.load('data/X_test.npy')
+y_test = np.load('data/y_test.npy')
+
+# initialize model
+knn_model = KNeighborsClassifier(n_neighbors=1)
+knn_model.fit(X_train, y_train)
+
+# save model
+pickle.dump(knn_model, open('knn_model.pkl','wb'))
+
+# predict on test set
+knn_pred = knn_model.predict(X_test)
+
+# evaluate
+accuracy = accuracy_score(y_test, knn_pred)
+precision = precision_score(y_test, knn_pred)
+recall = recall_score(y_test, knn_pred)
+
+
+# save metrics
+knn_metrics_dict={
+    'accuracy':accuracy,
+    'precision':precision,
+    'recall':recall
+}
+
+with open('knn_metrics.json', 'w') as file:
+    json.dump(knn_metrics_dict, file, indent=4)
