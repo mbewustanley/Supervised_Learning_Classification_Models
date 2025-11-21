e359618d4f75a2d66c512cbfbed47ed086899a53
mbewustanley <mbewustanley@gmail.com>
2025-11-21 16:07:27 +0100

    adding support vector machine model init, SVM model in pkl and SVM performance metrics

---
+++ src/Models/SVM.py
@@ -0,0 +1,39 @@
+import json
+import numpy as np
+import pickle
+
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score
+
+#import data
+X_train = np.load('data/X_train.npy')
+y_train = np.load('data/y_train.npy')
+X_test = np.load('data/X_test.npy')
+y_test = np.load('data/y_test.npy')
+
+# initialize model
+svm_model = SVC()
+svm_model.fit(X_train, y_train)
+
+# save modsvm
+pickle.dump(svm_model, open('svm_model.pkl','wb'))
+
+# predict on test set
+svm_pred = svm_model.predict(X_test)
+
+# evaluate
+accuracy = accuracy_score(y_test, svm_pred)
+precision = precision_score(y_test,svm_pred)
+recall = recall_score(y_test, svm_pred)
+
+
+# save metrics
+svm_metrics_dict={
+    'accuracy':accuracy,
+    'precision':precision,
+    'recall':recall
+}
+
+with open('svm_metrics.json', 'w') as file:
+    json.dump(svm_metrics_dict, file, indent=4)
